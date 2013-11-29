8cbeb2b254e2e701b80c2df130f9a13cb7d87b5c
Matjaz Muhic <matjazvn@gmail.com>
2013-11-29 23:17:36 +0100

    Adds succ and sum functions.

--- church.rb
+++ church.rb
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 
-toInt = ->(f) { f.(->(x) { x + 1}).(0) }
+toInt = ->(f) { f.(->(x) { x + 1 }).(0) }
+succ = ->(n) { ->(f) { ->(x) { f.((n.(f)).(x)) } } }
+plus = ->(m) { ->(n) { ->(f) { ->(x) { (m.(f)).((n.(f)).(x))  } } } }
 
 zero = ->(f) { ->(x) { x } }
 one = ->(f) { ->(x) { f.(x) } }
 two = ->(f) { ->(x) { f.(f.(x)) } }
+three = succ.(two)
+four = succ.(three)
+five = plus.(two).(three)
 
 r0 = toInt.(zero)
 r1 = toInt.(one)
 r2 = toInt.(two)
+r3 = toInt.(three)
+r4 = toInt.(four)
+r5 = toInt.(five)
 
-puts "Results: #{r0}, #{r1} and #{r2}"
+puts "Results: #{r0}, #{r1}, #{r2}, #{r3}, #{r4}, #{r5}"
 
